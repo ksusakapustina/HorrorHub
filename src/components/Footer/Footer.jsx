b1d2ef73649620d467d43f917f24881bb2be546b
Nia <ksusakapustina5@gmail.com>
2026-05-19 22:28:04 +0300

    Исправлена ошибка в футере

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import './Footer.css'
 
 const Footer = () => {
-    currentYear = new Date().getFullYear();
+   const currentYear = new Date().getFullYear();
 
     return (
         <footer className='footer'>
             <p className='footer-info'>
                 Ⓒ {currentYear} Хоррор-Хаб - Все права защищены | Сделано с ❤️ для любителей ужасов
             </p>
-            // МБ, ДОБАВИТЬ КОНТАКТНУЮ ИНФОРМАЦИЮ
+            {/* МБ, ДОБАВИТЬ КОНТАКТНУЮ ИНФОРМАЦИЮ */}
         </footer>
     );
 }
