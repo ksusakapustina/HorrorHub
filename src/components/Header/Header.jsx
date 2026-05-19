27b3b4bd852a257bc8a8f54e6d05cb8830ca7bc8
Nia <ksusakapustina5@gmail.com>
2026-05-19 21:20:50 +0300

    Добавила часть со скроллом

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,44 +5,51 @@ import './Header.css'
 const Header = () => {
     return (      
         <header className="header">
-            <a href="/" className="logo">
-                <span className="red">X</span>
-                <span className="white">X</span>
-            </a>                
-            <nav>
-                <ul>
-                    <li className="header_nav-link">
-                        <Link
-                            to="/main_page"
-                            className="header_nav-link-item"
-                        >
-                            ГЛАВНАЯ
-                        </Link>
-                    </li>
-                    <li className="header_nav-link">
-                        <Link
-                            to="/myths"
-                            className="header_nav-link-item"
-                        >
-                            МИФЫ                           
-                        </Link>
-                    </li>
-                    <li className="header_nav-link">
-                        <Link
-                            to="/true_crime"
-                            className="header_nav-link-item">
-                                ТРУ КРАЙМ
-                        </Link>
-                    </li>
-                    <li className="header_nav-link">
-                        <Link
-                            to="/kuplinov"
-                            className="header_nav-link-item">
-                                КУПЛИНОВ
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header-fixed">
+                <a href="/" className="logo">
+                    <span className="red">X</span>
+                    <span className="white">X</span>
+                </a>                
+                <nav>
+                    <ul>
+                        <li className="header_nav-link">
+                            <Link
+                                to="/main_page"
+                                className="header_nav-link-item"
+                            >
+                                ГЛАВНАЯ
+                            </Link>
+                        </li>
+                        <li className="header_nav-link">
+                            <Link
+                                to="/myths"
+                                className="header_nav-link-item"
+                            >
+                                МИФЫ                           
+                            </Link>
+                        </li>
+                        <li className="header_nav-link">
+                            <Link
+                                to="/true_crime"
+                                className="header_nav-link-item">
+                                    ТРУ КРАЙМ
+                            </Link>
+                        </li>
+                        <li className="header_nav-link">
+                            <Link
+                                to="/kuplinov"
+                                className="header_nav-link-item">
+                                    КУПЛИНОВ
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
+            <div className="scrollabele">
+                <h1>ХОРРОР-ХАБ</h1>
+                <h3>Твой портал в мир ужасов</h3>
+            </div>
         </header>
     );
 }
