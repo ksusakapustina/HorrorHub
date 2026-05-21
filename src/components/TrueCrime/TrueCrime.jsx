47f1bb92ce2b8e727b178988a28dcd65c5abefb6
Nia <ksusakapustina5@gmail.com>
2026-05-21 22:15:04 +0300

    Сделана главная страница

---
+++ src/components/TrueCrime/TrueCrime.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+import './TrueCrime.scss'
+
+const TrueCrime = () => {
+    return (
+        <div>
+            <p>SOME CONTENT</p>
+        </div>
+    )
+}
+
+export default TrueCrime;
