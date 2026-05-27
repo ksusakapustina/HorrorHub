67a5b6b45ff49063e0ebb4406cac79a17c5a3c34
Nia <ksusakapustina5@gmail.com>
2026-05-28 00:59:44 +0300

    Create Myths' header

---
+++ src/components/Myths/Myths.data.js
@@ -0,0 +1,38 @@
+import mythsHeadImage from "../../assets/images/myths/myths-header.jpg";
+import CtulhuImage from "../../assets/images/myths/ktulhu.jpg";
+import litfilmsImage from "../../assets/images/myths/litfilms.jpg";
+import mythsImage from "../../assets/images/myths/myths.jpg";
+import strangePlaceImage from "../../assets/images/myths/strange-places.png";
+
+export const mythsHead = {
+    title: "МИФЫ И ЛЕГЕНДЫ",
+    subtitle: "Древние ужасы, которые живут в\nколлективном подсознании человечества",
+    image: mythsHeadImage,
+};
+
+export const Ctulhu = {
+    eyebrow: "МИФЫ И ЛЕГЕНДЫ - УЖАС ИЗ ГЛУБИН ВСЕЛЕННОЙ",
+    title: "Мифы Ктулху: Когда древние боги пробуждаются",
+    text:
+            `Говард Филлипс Лавкрафт создал целую мифологию,
+            которая пугает уже практически сто лет.
+            Древние существа, спящие в глубинах океана и
+            между звёздами, чьё простое существование сводит
+            людей с ума. Йог-Сотот, Ньярлатотеп, Азатот -
+            имена, от которых кровь стынет. Философия космического
+            индифферентизма «мы ничто перед лицом бесконечной вселенной»
+            стала фундаментом всего жанра.`,
+    image: CtulhuImage,
+    imageAlt: "Ктулху, как его описывал Лавкрафт",
+}
+
+
+
+
+
+
+
+
+
+
+
